10f0c0bcd43a96a06a179f630ce9d047536bd647
Sai Haritha Kolluru <139159045+Kollurusaiharitha@users.noreply.github.com>
2024-01-31 14:43:48 +0530

    Add files via upload
    this is the code with GUI

--- guess the number .py
+++ guess the number .py
@@ -1,13 +1,21 @@
 from random import randint
+from tkinter import *
+from tkinter import simpledialog, messagebox
+
+main = Tk()
+main. withdraw()
 
 random_number = randint(0,10)
 print(random_number)
 x = -1
-x =  int(x)
 while x != random_number:
     if x != -1:
-        print("you guessed wrong")
-    x = input("Enter a number: ")
+       messagebox.showinfo("wrong", "wrong guess")
+    x = simpledialog.askstring("guesss","Enter a Numbers")
     x = int(x)
-    print("you guessed correctly")
-    
+
+    if x == random_number:
+        messagebox.showinfo("Right", "you guessed right")
+        exit()
+main.mainloop()
+   
